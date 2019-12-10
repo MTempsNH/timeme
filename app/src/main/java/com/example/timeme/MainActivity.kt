27b7e84a97cf80463810a8d9d968504c9535c85b
Mark Temple <mtemps@gmail.com>
2019-12-09 21:15:10 -0500

    Initial design learnings

--- app/src/main/java/com/example/timeme/MainActivity.kt
+++ app/src/main/java/com/example/timeme/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.timeme
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.widget.ImageButton
+
+import android.util.Log
 
 class MainActivity : WearableActivity() {
 
@@ -11,5 +14,19 @@ class MainActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Log.i("001", "test log")
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.btn_click_me) as ImageButton
+
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            Log.i("001", "button clicked")
+        }
     }
 }
